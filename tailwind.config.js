5bff398b71d6c40b013a2ffcf259ce51a3e58144
Eloy <eloycruzmartin@gmail.com>
2024-07-20 16:40:21 +0200

    fixing dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
      './Views/**/*.cshtml'
 ],
   theme: {
-      extend: {},
+      extend: {colors: {
+        primary: {
+          light: '#C3FCF2', // light primary color
+          DEFAULT: '#02B6A4', // primary color
+          dark: '#008070', // dark primary color
+        },
+        secondary: {
+          light: '#A78BFA', // light secondary color
+          DEFAULT: '#7C3AED', // secondary color
+          dark: '#4A1E96', // dark secondary color
+        },
+        accent: {
+          light: '#FFE6FF', // light accent color
+          DEFAULT: '#820081', // accent color
+          dark: '#B45309', // dark accent color
+        }
+      }
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 }
