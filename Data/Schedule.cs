88f784d09d689cfbe041d743a32cc885ade49e03
Proloyx <eloycruzmartin@gmail.com>
2024-07-27 11:45:02 +0200

    Changing language to spanish

--- Data/Schedule.cs
+++ Data/Schedule.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace Agenda.Data;
@@ -9,15 +10,15 @@ public partial class Schedule
     public int Scheduleid { get; set; }
 
     public int Centerid { get; set; }
-
+    [Display(Name = "Fecha")]
     public DateOnly Workdate { get; set; }
-
+    [Display(Name = "Entrada")]
     public TimeOnly Starttime { get; set; }
-
+    [Display(Name = "Salida")]
     public TimeOnly Endtime { get; set; }
-
+    [Display(Name = "Horas")]
     public decimal Workedhours { get; set; }
-
+    [Display(Name = "Descripción")]
     public string? Description { get; set; }
     [JsonIgnore]
     public virtual Workcenter Center { get; set; } = null!;
--- Data/Workcenter.cs
+++ Data/Workcenter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace Agenda.Data;
@@ -9,15 +10,15 @@ public partial class Workcenter
     public int Centerid { get; set; }
 
     public int Userid { get; set; }
-
+    [Display(Name = "Nombre")]
     public string Name { get; set; } = null!;
 
     public string? Address { get; set; }
-
+    [Display(Name = "Bruto")]
     public decimal Grossrate { get; set; }
-
+    [Display(Name = "Día de Cobro")]
     public int? Paymentday { get; set; }
-
+    [Display(Name = "Neto")]
     public decimal Netrate { get; set; }
 
     public virtual ICollection<Schedule> Schedules { get; set; } = new List<Schedule>();
